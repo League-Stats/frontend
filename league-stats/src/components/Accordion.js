1200d83a46c71580eecefc8cc457f23f293aca89
JaySebagh <jaysebagh@gmail.com>
2020-05-14 15:09:18 -0700

    still bugged

--- league-stats/src/components/Accordion.js
+++ league-stats/src/components/Accordion.js
@@ -31,25 +31,44 @@ function Accordion(props) {
     return showHours + showMinutes + showSeconds
   }
 
+  // async function getGameMode(queueId){
+  //   try{
+  //     const res = await axios.request({
+  //       method: 'GET',
+  //       url: "https://static.developer.riotgames.com/docs/lol/queues.json"
+  //     })
+  //     const data = res.data.find(mode => mode.queueId === queueId).description.slice(0, -6);
+  //     return data
+  //   } catch(error) {
+  //     console.log(error);
+  //   }
+  // }
+
   async function getGameMode(queueId){
-    try{
-      const res = await axios.request({
-        method: 'GET',
-        url: "https://static.developer.riotgames.com/docs/lol/queues.json"
-      })
-      const data = res.data.find(mode => mode.queueId === queueId).description.slice(0, -6);
-      console.log(data)
-      return data
-    } catch(error) {
-      console.log(error);
-    }
+    await axios.request({
+      method: 'GET',
+      url: "https://static.developer.riotgames.com/docs/lol/queues.json"
+    })
+    .then(res => {
+      return res.data
+    })
+    .then((res) =>{
+      const test = res.find(mode => mode.queueId === queueId).description.slice(0, -6)
+      return test.toString()
+    })
+    .catch(error => {
+      console.log(error)
+    })
   }
+
+  const gameTime = gameTimeConversion(m.gameDuration)
+  const gameMode = getGameMode(m.queueId);
 
   return (
     <div className="accordion-container">
       <button className={`${props.playerWin ? "accordion-win" : "accordion-loss"} ${setActive}`} onClick={toggleAccordion}>
-        {gameTimeConversion(m.gameDuration)}
-        {getGameMode(m.queueId)}
+        {gameTime}
+        {gameMode}
         <i className={`fas fa-chevron-right ${setRotate}`} />
       </button>
       <div
